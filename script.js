ee384c1addf9d93498caae2ff9658efcc7ade4f9
hp <sahalnurdin@gmail.com>
2022-11-03 20:28:38 +0700

    intial commit

---
+++ script.js
@@ -0,0 +1,109 @@
+// Single line Comment
+/*Multiline Comment */
+
+// Value and variable
+/*
+let js  = 'amazing';
+console.log(40+23+8-18);
+
+console.log("Sahal")
+console.log("19")
+
+let firstName = 'Muhammad';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Sahal_Nurdin = "SN";
+let $function = 27;
+
+let person = "Sahal";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Programmer";
+
+console.log(myCurrentJob);
+*/
+
+// Assigmment 
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+// Answer:
+/*
+let country = "Indonesia";
+let continent = "Asia";
+let population = 250000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+*/
+// let digunakan untuk membuat variable baru
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 19);
+console.log(typeof 'Sahal');
+
+javascriptIsFun = 'YES!!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2003;
+console.log(year);
+console.log(typeof year)
+
+console.log(typeof null);*/
+
+/* Assigment
+1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet 
+2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console */
+// Jawab:
+/*
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+// let, const, dan var 
+// const muncul di es6 (2015) es(ecmascript)
+/*
+let age = 19;
+age = 20;
+console.log(age)
+
+const birthday = 2003;
+*/
+// birthday = 2004;
+// console.log(birthday);
+/*
+var job = "Programmer";
+job = "Scientist";
+console.log(job);
+
+var lastName = "Nurdin";
+console.log(lastName);
+*/
+/* Assigment
+1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one) 
+2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const. 
+3. Try to change one of the changed variables now, and observe what happens */
+// Answer:
+/*
+language = "Bahasa Indonesia";
+const country = "Indonesia";
+const continent = "Asia";
+const isIsland = false;
+isIsland = true;
+*/
