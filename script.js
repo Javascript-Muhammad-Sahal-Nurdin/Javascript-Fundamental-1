c0ecd9903f845bc3746fa24b1c96c596b31bb663
hp <sahalnurdin@gmail.com>
2022-11-05 22:25:27 +0700

    add files

--- script.js
+++ script.js
@@ -27,22 +27,10 @@ let myCurrentJob = "Programmer";
 console.log(myCurrentJob);
 */
 
-// Assigmment 
-// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
-// 2. Log their values to the console
-// Answer:
-/*
-let country = "Indonesia";
-let continent = "Asia";
-let population = 250000000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
 
-*/
 // let digunakan untuk membuat variable baru
-/*
+
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -56,26 +44,15 @@ console.log(typeof javascriptIsFun);
 
 let year;
 console.log(year);
-console.log(typeof year);
+console.log(typeof year);    
 
 year = 2003;
 console.log(year);
 console.log(typeof year)
 
-console.log(typeof null);*/
+console.log(typeof null);
+console.log(typeof 18)
 
-/* Assigment
-1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet 
-2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console */
-// Jawab:
-/*
-let isIsland = false;
-let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-*/
 // let, const, dan var 
 // const muncul di es6 (2015) es(ecmascript)
 /*
