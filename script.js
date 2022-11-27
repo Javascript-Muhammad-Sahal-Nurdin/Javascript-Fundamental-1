973e5c960555cc824f6d6ed3f54e9840491bd048
hp <sahalnurdin@gmail.com>
2022-11-27 12:02:20 +0700

    add changes

--- script.js
+++ script.js
@@ -510,3 +510,22 @@ console.log(drink);
 
 // Bisa digunakan di template literals
 console.log(`I like to drink ${age >= 18 ? "wine 👌" : "water 👌"}`);
+
+// Coding Challenge #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%
+
+1. Your task is to calculate the tip, depending on the bill value. Create a variable called "tip" for this. It's not allowed to use an if/else statement (if it's easier for you, you can start with an if/else statement, and them try to convert it to a ternary operator!)
+
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40, and 430
+*/
+
+const billValue = Number(prompt("Masukkan bill: "));
+
+const tipCalc = billValue >= 50 && billValue <= 300 ?  billValue * (15 / 100)  : billValue * (30 / 100);  ;
+
+
+console.log(`The bill was ${billValue}, the tip was ${tipCalc}, and the total value ${billValue + tipCalc}`)
