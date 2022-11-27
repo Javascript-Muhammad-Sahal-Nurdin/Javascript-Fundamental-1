b82033a610eceba3629fd084932e20dc15769f96
hp <sahalnurdin@gmail.com>
2022-11-27 08:54:50 +0700

    add changes

--- script.js
+++ script.js
@@ -356,7 +356,7 @@ if (favourite !== 23) console.log("Why not 23? ");
 // Boolean Logic
 /* Penjelasan ada di materi dan video
 
-*/
+
 
 const hasDriversLicense = true;
 const hasGoodVision = true;
@@ -381,4 +381,37 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
     console.log("Sarah is able to drive!");
 } else {
     console.log("Someone else should drive...");
+}
+*/
+
+// Coding Challenge 3
+
+/* 
+There are two gymnastic teams, Dolphins and Koalas, they compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below 
+2. Compare the team's average scores to determine the winner of the competition and print it to the console. Don't forget that there be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirements for a minimum score of 100. With this rule, a team only wins if it has a higher score of at least 100 points , HINT : Use a logical operator to test for minimum score, as well as multiple else-if blocks.
+
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+
+Test Data: Dolpins score 96, 108, and 89. Koalas score 88, 91, and 110.
+TEST DATA BONUS 1: Dolpins score 97, 112, and 101. Koalas score 109, 95, and 123.
+TEST DATA BONUS 2: Dolpins score 97, 112, and 101. Koalas score 109, 95, and 106.
+*/
+
+const dolpinsScore = 96 + 108 + 89;
+const koalasScore = 88 + 91 + 110;
+
+const scoreAverageDolphins = dolpinsScore / 3;
+const scoreAverageKoalas = koalasScore / 3;
+
+if (scoreAverageDolphins >= scoreAverageKoalas) {
+    console.log("Dolphins Wins!!!");
+} else if (scoreAverageDolphins <= scoreAverageKoalas){
+    console.log("Koalas Wins!!!");
+} else {
+    console.log("Draw !!!");
 }
